c9153a684b14f0c700040c3926c86333e253d7c0
Peter Rhodes <petrov82@aim.com>
2014-03-25 11:29:18 -0500

    edit navbar

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -54,12 +54,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Bootstrap theme</a>
+          <a class="navbar-brand" href="{{ Request::is('blog') ? 'active' : '' }}">Peter J. Rhodes</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="{{ Request::is('resume') ? 'active' : '' }}">Resume</a></li>
+            <li><a href="{{ Request::is('portfolio') ? 'active' : '' }}">Portfolio</a></li>
             <li><a href="#contact">Contact</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
