e23fc843f494bb2d178431832003e74c23339f02
Peter Rhodes <petrov82@aim.com>
2014-03-25 11:28:29 -0500

    move all functions from route to this file

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -20,11 +20,68 @@ class HomeController extends BaseController {
 		return View::make('hello');
 	}
 
+	public function redirectWP()
+	{
+		return Redirect::to('http://peterrhodes.net');
+	}
+
+	public function sayGoodbye()
+	{
+		return ('Goodbye!');
+	}
+
+	public function helloCodeup()
+	{
+		return "Hello, CodeUp!";
+	}
+
 	public function sayHello($name)
 	{
-	$data = array('name' => $name);
+		$data = array('name' => $name);
 
-	return View::make('my-first-view')->with($data);
+		return View::make('my-first-view')->with($data);
+	}
+
+	public function showResume()
+	{
+		return View::make('resume');
+	}
+
+	public function showPortfolio()
+	{
+		return View::make('portfolio');
+	}
+
+	public function rollDice($guess = null)
+	{
+		$roll = mt_rand(1, SIDES_OF_DICE);
+
+		$didroll = "Have";
+
+		$result = "";
+
+		if (is_null($guess) || (!is_numeric($guess)))
+		{
+			$didroll = "Have Not";
+			$result = "You must guess a number, or be cast into the gorge of eternal peril! You could have guessed $roll!";
+		}
+		elseif ($guess == $roll) 
+		{
+			$result = "You have guessed correctly, padawan! $guess is the correct number!";
+		}
+		else
+		{
+			$result = "You have chosen $guess unwisely. The correct answer is $roll.";
+		}
+
+		$data = array(
+			'guess' => $guess,
+			'result' => $result,
+			'roll' => $roll,
+			'didroll' => $didroll
+		);
+
+		return View::make('rolldice')->with($data);
 	}
 
 }
