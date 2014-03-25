cc40237d470f430ed119f654b27f3a5313fc920d
Peter Rhodes <petrov82@aim.com>
2014-03-25 15:59:19 -0500

    error pages

---
+++ app/views/errors/abort.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title')
+Abort! 403
+ @stop
+
+@section('content')
+<h1>Oops! I couldn't complete that command.<small>&nbsp;/*403*/</small></h1>
+@stop
---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title')
+Page Not Found! 404
+ @stop
+
+@section('content')
+<h1>Oops! Cannot Find that Page<small>&nbsp;/*404*/</small></h1>
+@stop
