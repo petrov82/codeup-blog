8c882f64215119fe1f1012fbe14f343c3e5aed3d
petrov82 <petrov82@aim.com>
2014-04-02 19:29:27 -0500

    show author username

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -28,7 +28,7 @@
 				<div class="blog-post">
 					<h2 class="blog-post-title"><a href="{{{ action('PostsController@show', $post->id)}}}">{{{ $post->title }}}</a></h2>
 					<p>
-						<small>Author: {{{ $post->user->email }}}</small>
+						<small>Author: {{{ $post->user->user_name }}}</small>
 					</p>
 					<p>
 						<small>Last Updated at: {{{ $post->updated_at->setTimezone('America/Chicago')->format('l, M j, Y @ h:i A') }}}</small>
