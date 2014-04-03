c49c619048d3ed6119c6913b0896c2358e208874
petrov82 <petrov82@aim.com>
2014-04-03 15:13:55 -0500

    add set role id to roles in seeder

--- app/database/seeds/RoleTableSeeder.php
+++ app/database/seeds/RoleTableSeeder.php
@@ -7,14 +7,17 @@ class RoleTableSeeder extends Seeder {
         DB::table('roles')->delete();
 
         $role = new Role();
+        $role->id   = '1';
         $role->role = 'Administrator';
         $role->save();
 
         $role = new Role();
         $role->role = 'Author';
+        $role->id   = '2';
         $role->save();
 
         $role = new Role();
+        $role->id   = '3';
         $role->role = 'Contributor';
         $role->save();
     }
