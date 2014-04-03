f396aaacbb7f199358d9822d5fd6327896df9e60
petrov82 <petrov82@aim.com>
2014-04-03 16:05:31 -0500

    ParseDown!

--- app/config/app.php
+++ app/config/app.php
@@ -107,6 +107,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'MaxHoffmann\Parsedown\ParsedownServiceProvider',
 
 	),
 
@@ -173,6 +174,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Markdown'        => 'MaxHoffmann\Parsedown\ParsedownFacade',
 
 	),
 
